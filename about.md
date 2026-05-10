92c14ff2deecc62c75bb475199ce53ce023e3750
miskaa <office.misko@gmail.com>
2026-05-10 14:04:47 +0200

    thy initial commit good sir

---
+++ about.md
@@ -0,0 +1,4 @@
+# <cr>THIS MOD DOESNT RECORD ANYTHING ITS A JOKE MOD</c>
+anyways this is just a silly mod thats just like Unregistered Hypercam but instead its Bandicam
+
+## The main <cr>Bandicam</c> website is [this](https://www.bandicam.com/), this mod isnt associated with the <cb>Bandicam Company</c>
---
+++ src/main.cpp
@@ -0,0 +1,96 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+/*
+        auto enabled = Mod::get()->getSettingValue<bool>("enabled");
+        if (enabled) {
+            auto transparency = Mod::get()->getSettingValue<float_t>("transparency");
+            auto winSize = CCDirector::sharedDirector()->getWinSize();
+
+            m_fields->m_bSpr = CCSprite::create("bandicam.png"_spr);
+
+            if (m_fields->m_bSpr) {
+                m_fields->m_bSpr->setAnchorPoint({0.5f,1.0f});
+                m_fields->m_bSpr->setPosition({winSize.width/2.0f,winSize.height});
+                m_fields->m_bSpr->setZOrder(42069);
+                m_fields->m_bSpr->  setOpacity(transparency);
+
+                this->addChild(m_fields->m_bSpr);
+            }
+        }
+*/
+
+#define SHIT 42069
+
+CCSprite* getw() {
+    auto overlay = OverlayManager::get();
+    return static_cast<CCSprite*>(overlay->getChildByTag(SHIT));
+}
+
+void bscreate() {
+    auto shit = Mod::get();
+    auto overlay = OverlayManager::get();
+
+    if (!shit->getSettingValue<bool>("enabled")) return;
+
+    if (getw()) return;
+
+    auto ws = CCDirector::sharedDirector()->getWinSize();
+    auto transp = shit->getSettingValue<float_t>("transparency");
+
+    auto spr = CCSprite::create("bandicam.png"_spr);
+    if (!spr) return;
+
+    spr->setTag(SHIT);
+    spr->setAnchorPoint({0.5f, 1.0f});
+    spr->setPosition({ws.width / 2.0f, ws.height});
+    spr->setZOrder(42069);
+    spr->setOpacity(transp);
+
+    overlay->addChild(spr);
+}
+
+$on_mod(Loaded) {
+    bscreate();
+
+    listenForSettingChanges<float_t>("transparency", [](float_t val) {
+        if (auto spr = getw()) {
+            spr->setOpacity(val);
+        }
+    });
+
+    listenForSettingChanges<bool>("enabled", [](bool val) {
+        if (auto spr = getw()) {
+            spr->setVisible(val);
+        } else if (val) {
+            bscreate();
+        }
+    });
+}
+
+/*
+
+class $modify(PlayLayer) {
+    bool init(GJGameLevel* level, bool useReplay, bool dontSave) {
+        if (!PlayLayer::init(level, useReplay, dontSave)) return false;
+        bscreate();
+        return true;
+    }
+};
+class $modify(LevelEditorLayer) {
+  bool init(GJGameLevel* level, bool p1) {
+      if (!LevelEditorLayer::init(level, p1)) return false;
+      bscreate();
+      return true;
+  }
+};
+class $modify(MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
+        bscreate();
+        return true;
+    }
+};
+
+*/
